da5232841ccf60a0b0c8e85ee7402d4bfcc2b2b5
GuybsonDev <guybsonlopescomercial@gmail.com>
2025-03-16 20:04:15 -0300

    Envio de arquivos iniciais

--- backend/ifood_dashboard/wsgi.py
+++ backend/ifood_dashboard/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for YourDashBackend project.
+WSGI config for ifood_dashboard project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YourDashBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ifood_dashboard.settings')
 
 application = get_wsgi_application()
